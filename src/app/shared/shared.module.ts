46effb955a2698d05534d7794bd382a8908d9756
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-24 14:29:23 -0600

    delete pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomMaterialModule } from '../custom-material/custom-material.module';
-import { LimitToPipe } from './pipes/limit-to.pipe';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { ContentPlaceholderAnimationComponent } from './content-placeholder-animation/content-placeholder-animation.component';
-import { LocalDatePipe } from './pipes/local-date.pipe';
-import { YesNoPipe } from './pipes/yes-no.pipe';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
@@ -22,9 +19,6 @@ import { LayoutComponent } from './layout/layout.component';
   declarations: [
     ConfirmDialogComponent,
     ContentPlaceholderAnimationComponent,
-    LimitToPipe,
-    LocalDatePipe,
-    YesNoPipe,
     LayoutComponent
   ],
   exports: [
@@ -32,11 +26,8 @@ import { LayoutComponent } from './layout/layout.component';
     ReactiveFormsModule,
     FlexLayoutModule,
     CustomMaterialModule,
-    LimitToPipe,
     ConfirmDialogComponent,
     ContentPlaceholderAnimationComponent,
-    LocalDatePipe,
-    YesNoPipe
   ],
   entryComponents: [
     ConfirmDialogComponent
