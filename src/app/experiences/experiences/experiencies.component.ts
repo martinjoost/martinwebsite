a76661ff34c05d9ad1151e66c661de6c7efbb49d
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-25 13:49:46 -0600

    fix card max width

--- src/app/experiences/experiences/experiencies.component.ts
+++ src/app/experiences/experiences/experiencies.component.ts
@@ -17,7 +17,7 @@ export class ExperienciesComponent implements OnInit {
       position:'Frontend Developer',
       implementations: ['Angular 9', 'Angular Universal','i18n', 'Progressive Web App (PWA)', 'NODE.JS', 'ROUTES', 'MODULES', 'RXJS', 'NGRX STORE', 'REACTIVE FORMS', 'MANEJO DE TOKENS'],
       description:'En este interesante proyecto participé en el FrontEnd llevando a cabo mis capacidades conocimientos y habilidades en Angular en su versión 9 siendo parte de un buen equipo de desarrollo ágil con Bitbucket, Jira confluence. Llevé a cabo ciertas librerías e implementaciones exitosamente a lo largo de este proyecto: Angular UNIVERSAL (SSA). I18n Internationalization. Progressive Web App (PWA).Módulos, rutas, guards, pipes, operadores rxjs, ngrx. Manejo de peticiones API Restful por manejo de modelos. Validaciones seguras de tokens. Formularios reactivos, validadores del mismo',
-      url:'./assets/images/trillonario.jpg',
+      url:'../../../assets/images/trillonario.jpg',
       from:'',
       to:''
     }, 
@@ -28,7 +28,7 @@ export class ExperienciesComponent implements OnInit {
       position:'Frontend Developer',
       implementations: ['AngularJS', 'VB.NET', 'HighCharts.js','Oracle DB', 'Soporte técnico presencial','Reportes', 'Mantención Sistema','Procedimientos Almacenados', 'Exposición de prototipos funcionales'],
       description:'Mantención del sistema, manipulación de información confidencial. Creación de procedimientos y tablas almacenadas en Oracle DB. Desarrollo de herramientas innovadoras para el sistema WebApp de Hoja de Vida Digital del ejército, creada con AngularJS, Visual Basic, y Oracle DB. Este sistema va dirigido hacia todo personal militar registrados vigentes e históricos de Chile, por consecuencia manipulando datos confidenciales.',
-      url:'./assets/images/ejercito.jpg',
+      url:'../../../assets/images/ejercito.jpg',
       from:'Octubre 2018',
       to:'Diciembre 2019'
     }, 
@@ -39,7 +39,7 @@ export class ExperienciesComponent implements OnInit {
       position:'Asistente informático',
       implementations: [],
       description:'Emprendimiento familiar participando como asistente del docente en algunas capacitaciones para distintos lenguajes y frameworks',
-      url:'./assets/images/chileforma.jpg',
+      url:'../../../assets/images/chileforma.jpg',
       from:'Enero 2017',
       to:'Noviembre 2018'
     }
