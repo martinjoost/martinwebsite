bb3f9e3bf95e5f9361d6e169eea5836410f33c67
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-25 22:29:31 -0600

    add experiences

--- src/app/experiences/experiences/experiencies.component.ts
+++ src/app/experiences/experiences/experiencies.component.ts
@@ -24,7 +24,8 @@ export class ExperienciesComponent implements OnInit {
     {
       name:'Ejército de Chile',
       remote: false,
-      location:'Edificio Ejercito bicentenario',
+      locationUrl: 'https://www.google.com/maps/place/Edificio+Ejercito+Bicentenario/@-33.4566079,-70.6609026,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c53b8dce2a25:0xb60df989bd9e2314!8m2!3d-33.4566079!4d-70.6587139',
+      location:'Edificio Ejército bicentenario',
       position:'Frontend Developer',
       implementations: ['AngularJS', 'VB.NET', 'HighCharts.js','Oracle DB', 'Soporte técnico presencial','Reportes', 'Mantención Sistema','Procedimientos Almacenados', 'Exposición de prototipos funcionales'],
       description:'Mantención del sistema, manipulación de información confidencial. Creación de procedimientos y tablas almacenadas en Oracle DB. Desarrollo de herramientas innovadoras para el sistema WebApp de Hoja de Vida Digital del ejército, creada con AngularJS, Visual Basic, y Oracle DB. Este sistema va dirigido hacia todo personal militar registrados vigentes e históricos de Chile, por consecuencia manipulando datos confidenciales.',
@@ -33,11 +34,12 @@ export class ExperienciesComponent implements OnInit {
       to:'Diciembre 2019'
     }, 
     {
-      name:'Chileforma',
+      name:'Chileforma Capacitaciones',
+      locationUrl: 'https://www.google.com/maps/place/Corp+de+Educacion+18+de+Septiembre/@-33.4470287,-70.6558626,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c509d0016231:0x352285dbd8ac455b!8m2!3d-33.4470332!4d-70.6536739',
       remote: false,
       location:'Av. 18 de Septiembre #1881',
       position:'Asistente informático',
-      implementations: [],
+      implementations: ['html5','angularjs','css3','javascript', 'soporte técnico', 'bootstrap','instalación de programas'],
       description:'Emprendimiento familiar participando como asistente del docente en algunas capacitaciones para distintos lenguajes y frameworks',
       url:'../../../assets/images/chileforma.jpg',
       from:'Enero 2017',
